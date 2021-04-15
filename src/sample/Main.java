4e5f42e9236b6d37d6cacfd7fea8faa864b197c6
Jirakan Koollasing <TerKrub084@gmail.com>
2021-04-15 19:21:04 +0700

    asd

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello");
+        primaryStage.setTitle("abcdef");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
