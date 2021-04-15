a1a39a76f74cef220e7922dfa2ef414bf00dc4e0
Very ArmandoZ <gorngorn9876@hotmail.com>
2021-04-15 19:14:16 +0700

    ppap

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("OHifgojfH");
+        primaryStage.setTitle("OAOQH");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
