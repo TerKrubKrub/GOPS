7f0f36b36fb221361a9eaa9a50ca69702005de3d
Very ArmandoZ <gorngorn9876@hotmail.com>
2021-04-15 19:13:30 +0700

    nananaa

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("OHH");
+        primaryStage.setTitle("OHifgojfH");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
