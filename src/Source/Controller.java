3087cb75d87383096e197d9e6d853b8ebced7471
gchax <pcg.mega@gmail.com>
2021-04-21 03:03:38 +0700

    style2

--- src/Source/Controller.java
+++ src/Source/Controller.java
@@ -1,6 +1,7 @@
 package Source;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -8,7 +9,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import Source.Main;
 
 import java.io.IOException;
 import java.net.URL;
@@ -18,7 +21,8 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
-    private Button startButton, tutorialButton, scoreButton, aboutButton, exitButton, preGameDoneButton;
+    private Button startButton, tutorialButton, scoreButton, aboutButton, exitButton,
+            preGameDoneButton, preGameBackButton, minimizeButton, closeButton;
     @FXML
     private ChoiceBox<String> botSelection = new ChoiceBox<String>();
 
@@ -28,27 +32,65 @@ public class Controller implements Initializable {
         botSelection.setValue("1 Bot");
     }
 
+    public void moveWindow(Parent root, Stage window) {
+        final double[] xOffset = {0};
+        final double[] yOffset = {0};
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset[0] = event.getSceneX();
+                yOffset[0] = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                window.setX(event.getScreenX() - xOffset[0]);
+                window.setY(event.getScreenY() - yOffset[0]);
+            }
+        });
+    }
+
     public void pressStart() throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("PreGame.fxml")));
         Stage window = (Stage) startButton.getScene().getWindow();
+        moveWindow(root, window);
         window.setScene(new Scene(root));
     }
 
-    public void pressTutorial() {
+    public void pressTutorial() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Tutorial.fxml")));
+        Stage window = (Stage) tutorialButton.getScene().getWindow();
+        moveWindow(root, window);
+        window.setScene(new Scene(root));
     }
 
-    public void pressScore() {
+    public void pressScore() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Score.fxml")));
+        Stage window = (Stage) scoreButton.getScene().getWindow();
+        moveWindow(root, window);
+        window.setScene(new Scene(root));
     }
 
-    public void pressAbout(ActionEvent actionEvent) {
+    public void pressAbout() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("About.fxml")));
+        Stage window = (Stage) aboutButton.getScene().getWindow();
+        moveWindow(root, window);
+        window.setScene(new Scene(root));
     }
 
-    public void pressExit() {
-        System.exit(0);
+    public void pressClose() {
+        Stage window = (Stage) closeButton.getScene().getWindow();
+        window.close();
+    }
+
+    public void pressMinimize() {
+        Stage window = (Stage) minimizeButton.getScene().getWindow();
+        window.setIconified(true);
     }
 
     public void pressPreGameDone() throws IOException {
-
         if (botSelection.getValue().toString().equals("1 Bot")) {
             System.out.println("1 Bot");
         } else if (botSelection.getValue().toString().equals("2 Bots")) {
@@ -56,6 +98,16 @@ public class Controller implements Initializable {
         } else if (botSelection.getValue().toString().equals("3 Bots")) {
             System.out.println("3 Bots");
         }
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(".fxml")));
+        Stage window = (Stage) preGameDoneButton.getScene().getWindow();
+        moveWindow(root, window);
+        window.setScene(new Scene(root));
     }
 
+    public void pressPreGameBack() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Menu.fxml")));
+        Stage window = (Stage) preGameBackButton.getScene().getWindow();
+        moveWindow(root, window);
+        window.setScene(new Scene(root));
+    }
 }
--- src/Source/Main.java
+++ src/Source/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.util.Objects;
 
@@ -14,14 +15,32 @@ public class Main extends Application {
     @Override
     public void start(Stage window) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Menu.fxml")));
+        Scene scene = new Scene(root, 1000, 700);
+        window.setScene(scene);
+        window.setTitle("GOPS: Shuffled!");
+        window.initStyle(StageStyle.TRANSPARENT);
+        moveWindow(root, window);
         Image icon = new Image("/Resource/icon.png");
         window.getIcons().add(icon);
-        window.setTitle("GOPS: Shuffled!");
-        window.setScene(new Scene(root, 1000, 700));
         window.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    public void moveWindow(Parent root, Stage window) {
+        final double[] xOffset = {0};
+        final double[] yOffset = {0};
+
+        root.setOnMousePressed(event -> {
+            xOffset[0] = event.getSceneX();
+            yOffset[0] = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            window.setX(event.getScreenX() - xOffset[0]);
+            window.setY(event.getScreenY() - yOffset[0]);
+        });
+    }
+
 }
